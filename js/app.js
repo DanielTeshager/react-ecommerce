b7cefa5bd4197b21d200e639360781122d0f5fc5
Daniel <dwolela@gmail.com>
2022-04-23 15:58:37 +0400

    Final commit before refactoring

--- js/app.js
+++ js/app.js
@@ -4,17 +4,16 @@ const minus = document.querySelector('.minus');
 const empty = document.querySelector('.empty');
 const item_count = document.querySelector('.number');
 const cart_quantity_badge = document.querySelector('.cart-quantity-badge');
-
 const cart_total = document.querySelector('.cart-total');
-
 const cart_items = document.querySelector('.cart-items');
-// const cart = JSON.parse(localStorage.getItem('cart')) || [];
 const big_product_image = document.querySelector('.big-product-image');
+
 //item name extraction from header 
 const item_name = document.querySelector('.header h1');
 
 //price extraction from 2nd span    
 const item_price = parseInt(document.querySelector('.price').children[1].textContent);
+
 //define max quantity
 let max_quantity = 9;
 const remove_icon = 'http://127.0.0.1:5500/images/icon-delete.svg';
@@ -26,9 +25,7 @@ const current_cart = {
 
 //populate cart
 function populateCart(cart) {
-    // cart_items.innerHTML = '';
-    // cart_total.textContent = '';
-    // cart_quantity.textContent = '';
+
     if (cart.item_quantity == 0) {
         cart_items.innerHTML = '';
         empty.style.display = 'none';
@@ -46,18 +43,12 @@ function populateCart(cart) {
    
 }
 
-
-
 //toggle cart 
 cart_icon.addEventListener('click', function() {
     document.querySelector('.cart-content').classList.toggle('show-cart');
 });
 
 //add item to cart
-
-
-//remove item from cart
-
 
 //increase quantity
 plus.addEventListener('click', function() {
@@ -107,42 +98,112 @@ cart_items_container.addEventListener('click', function(e) {
 //handle showcase lightbox
 const showcase = document.querySelector('#showcase');
 const lightbox = document.querySelector('#lightbox');
+
 //dispaly the lightbox
 showcase.addEventListener('click', function(e) {
+    console.log(e.target);
     if(e.target.classList.contains('showcase-img')) {
         document.querySelector('.lightbox').classList.add('show-lightbox');
         document.querySelector('.lightbox img').src = e.target.src;
     }
-    //show the lightbox
-    document.querySelector('#lightbox').classList.remove('hide');
+    // show the lightbox if media query is greater than 768px
+    if(window.matchMedia('(min-width: 768px)').matches) {
+     document.querySelector('#lightbox').classList.remove('hide');
+    }
 });
 
 // close the lightbox using the close-lightbox button
 document.querySelector('.close-lightbox').addEventListener('click', function() {
-    document.querySelector('#lightbox').classList.add('hide');
+    lightbox.classList.add('hide');
 });
 
 //next and previous images in lightbox
 document.querySelector('.next').addEventListener('click', function() {
-    let current = document.querySelector('#lightbox .big-product-image').src;
+
+    let current = document.querySelector('.big-product-image').src;
     let base = current.substring(0, current.lastIndexOf('/') + 1);
         current = current.split('/')[current.split('/').length - 1];
-    console.log(current);
     let next = current.replace(/[0-9]/g, function(i) {
         return i === '4' ? '1' : ++i;
     });
+    //update the image source of the main lightbox image
+    document.querySelector('.big-product-image').src = base+''+next;
+    let next_thumbnail = (next.split('.')[0]+'-thumbnail.jpg');
+    let prev_thumbail = (current.split('.')[0]+'-thumbnail.jpg');
+    //add active class to the next thumbnail
+    let parent_li = document.querySelector(`ul li img[src="./images/${next_thumbnail}"]`).parentElement;
+    parent_li.classList.add('active');
+    //remove active class from previous image container li
+    document.querySelector(`ul li img[src="./images/${prev_thumbail}"]`).parentElement.classList.remove('active');
+
+});
+
+document.querySelector('.lnext').addEventListener('click', function() {
+
+    let current = document.querySelector('#lightbox .big-product-image').src;
+    let base = current.substring(0, current.lastIndexOf('/') + 1);
+        current = current.split('/')[current.split('/').length - 1];
+    let next = current.replace(/[0-9]/g, function(i) {
+        return i === '4' ? '1' : ++i;
+    });
+
+    //update the image source of the main lightbox image
     document.querySelector('#lightbox .big-product-image').src = base+''+next;
     let next_thumbnail = (next.split('.')[0]+'-thumbnail.jpg');
     let prev_thumbail = (current.split('.')[0]+'-thumbnail.jpg');
-    console.log('next', next_thumbnail);
-    let parent = document.querySelector(`#lightbox ul li img[src="./images/${next_thumbnail}"]`).parentElement;
-    parent.classList.add('active');
-    //remove active class from previous image
+
+    //add active class to the next thumbnail
+    let parent_li = document.querySelector(`#lightbox ul li img[src="./images/${next_thumbnail}"]`).parentElement;
+    parent_li.classList.add('active');
+
+    //remove active class from previous image container li
     document.querySelector(`#lightbox ul li img[src="./images/${prev_thumbail}"]`).parentElement.classList.remove('active');
-    // change active tile
-    // document.querySelector('#lightbox .tiny-images .active').classList.remove('active');
-    // select li by it's child image attribute
-    // document.querySelector('#lightbox .tiny-images li[data-image="'+next+'"]').classList.add('active');
-//    console.log(document.querySelector('#lightbox img[src="'+base+''+(next.split('.')[0])+'-thumbnail.jpg"]'));
+
 });
 
+//handle previous image in lightbox and mobile view
+document.querySelector('.prev').addEventListener('click', function() {
+
+    let current = document.querySelector('.big-product-image').src;
+    let base = current.substring(0, current.lastIndexOf('/') + 1);
+        current = current.split('/')[current.split('/').length - 1];
+    let next = current.replace(/[0-9]/g, function(i) {
+        return i === '1' ? '4' : --i;
+    });
+
+    //update the image source of the main lightbox image
+    document.querySelector('.big-product-image').src = base+''+next;
+    let next_thumbnail = (next.split('.')[0]+'-thumbnail.jpg');
+    let prev_thumbail = (current.split('.')[0]+'-thumbnail.jpg');
+
+    //add active class to the next thumbnail
+    let parent_li = document.querySelector(`ul li img[src="./images/${next_thumbnail}"]`).parentElement;
+    parent_li.classList.add('active');
+
+    //remove active class from previous image container li
+    document.querySelector(`ul li img[src="./images/${prev_thumbail}"]`).parentElement.classList.remove('active');
+
+});
+
+document.querySelector('.lprev').addEventListener('click', function() {
+
+    let current = document.querySelector('#lightbox .big-product-image').src;
+    let base = current.substring(0, current.lastIndexOf('/') + 1);
+        current = current.split('/')[current.split('/').length - 1];
+    let next = current.replace(/[0-9]/g, function(i) {
+        return i === '1' ? '4' : --i;
+    });
+
+    //update the image source of the main lightbox image
+    document.querySelector('#lightbox .big-product-image').src = base+''+next;
+    let next_thumbnail = (next.split('.')[0]+'-thumbnail.jpg');
+    let prev_thumbail = (current.split('.')[0]+'-thumbnail.jpg');
+    
+    //add active class to the next thumbnail
+    let parent_li = document.querySelector(`#lightbox ul li img[src="./images/${next_thumbnail}"]`).parentElement;
+    parent_li.classList.add('active');
+    
+    //remove active class from previous image container li
+    document.querySelector(`#lightbox ul li img[src="./images/${prev_thumbail}"]`).parentElement.classList.remove('active');
+
+});
